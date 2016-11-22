c93587cc325b5ce2ec1c3638261547a621849beb
Malik Kasmi <malik.kasmi@gmail.com>
2016-11-22 16:56:35 +0100

    create routes

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,22 +1,39 @@
 class ArticlesController < ApplicationController
   def index
+    @articles = Article.all
   end
 
   def show
+    @article = Article.find(params[:id])
   end
 
   def new
+    @article = Article.new
   end
 
   def create
+    @article = Article.new(article_params)
+    @article.save
   end
 
   def update
+    @article = Article.find(params[:id])
+    @article.update(article_params)
   end
 
   def edit
+    @article = Article.find(params[:id])
   end
 
   def destroy
   end
+
+private
+
+def article_params
+    # *Strong params*: You need to *whitelist* what can be updated by the user
+    # Never trust user data!
+    params.require(:article).permit(:title, :content)
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,15 @@
 Rails.application.routes.draw do
-  get 'articles/index'
+  get    "articles",          to: "articles#index"
 
-  get 'articles/show'
+  get    "articles/:id",      to: "articles#show"
 
-  get 'articles/new'
+  get    "articles/new",      to: "articles#new"
+  post   "articles",          to: "articles#create"
 
-  get 'articles/create'
+  get    "articles/:id/edit", to: "articles#edit"
+  patch  "articles/:id",      to: "articles#update"
 
-  get 'articles/update'
-
-  get 'articles/edit'
-
-  get 'articles/destroy'
+  delete "articles/:id",      to: "articles#destroy"
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
